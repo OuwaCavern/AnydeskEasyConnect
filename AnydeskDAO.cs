90f470154149dca678819e4cd8978797d98de6c2
Raxzero <emircan.acikgoz@hotmail.com>
2024-04-03 17:28:23 +0300

    last update from my workplace

---
+++ AnydeskDAO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Microsoft.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace AnydeskEasyConnect
+{
+    internal class AnydeskDAO
+    {
+        private string ConnectionString = "";
+
+        static string CreateConnectionString()
+        {
+            try
+            {
+                ExeConfigurationFileMap fileMapping = new ExeConfigurationFileMap
+                {
+                    ExeConfigFilename = @"C:\Users\emirc\source\repos\AnydeskEasyConnect\connectionstring.config"
+                };
+                Configuration configuration = ConfigurationManager.OpenMappedExeConfiguration(fileMapping, ConfigurationUserLevel.None);
+                return configuration.AppSettings.Settings["baglanti"].Value;
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+                return "";
+            }
+        }
+        public List<Anydesks> MaydonozSiparisleriniGetir()
+        {
+            List<Anydesks> returnThese = new List<Anydesks>();
+            try
+            {
+                ConnectionString = CreateConnectionString();
+                string selectMaydonozAll = "SELECT SubeAdi,SubeAnydeskNumarasi,SubeAnydeskParolasi,BilgisayarYetkisi,OnlineStatus FROM Maydonoz";
+                SqlConnection sqlConnection;
+                sqlConnection = new SqlConnection(ConnectionString);
+                sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand(selectMaydonozAll, sqlConnection);
+                using (SqlDataReader reader = sqlCommand.ExecuteReader())
+                {
+
+                }
+
+            }
+        }
+
+    }
+}
---
+++ Anydesks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnydeskEasyConnect
+{
+    public class Anydesks
+    {
+        public string SubeAdi { get; set; }
+        public string SubeAnydeskNumarasi { get; set; }
+        public string SubeAnydeskParolasi { get; set; }
+        public string BilgisayarYetkisi {  get; set; }
+        public string OnlineStatus { get; set; }
+    }
+}
