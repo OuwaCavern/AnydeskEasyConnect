f61ac14df92b4c8e4a501568407ef0b66a9babd9
OuwaCavern <emircan.acikgoz@hotmail.com>
2024-07-14 21:21:46 +0300

    trimmed some excess code in editanydesk function

--- Login.cs
+++ Login.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace AnydeskEasyConnect
 {
@@ -28,6 +30,33 @@ namespace AnydeskEasyConnect
         private void KullaniciParolasiGirdisi_TextChanged(object sender, EventArgs e)
         {
             kullaniciParolasi = KullaniciParolasiGirdisi.Text;
+        }
+
+        private void GirisYapButonu_Click(object sender, EventArgs e)
+        {
+            string connectionString = AnydeskDAO.CreateConnectionString();
+            string selectQuery = $"SELECT * FROM UserBase WHERE KullaniciAdi='{PrivateHasher.ParolaSifreleyici(kullaniciAdi)}'";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(selectQuery, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                List<Dictionary<string, object>> result = new();
+
+                while (reader.Read())
+                {
+                    Dictionary<string, object> row = new();
+
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        row.Add(reader.GetName(i), reader.GetValue(i));
+                    }
+
+                    result.Add(row);
+                }
+            }
         }
     }
 }
